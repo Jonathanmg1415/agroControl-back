c7d15641b57a7afb988aae7082b5cefe111de450
Jonathanmg1415 <53263709+Jonathanmg1415@users.noreply.github.com>
2024-05-15 14:39:44 -0500

    Cmabio contraseña db

--- config/datastores.js
+++ config/datastores.js
@@ -58,7 +58,7 @@ module.exports.datastores = {
 
   agrocontroldb: {
     adapter: 'sails-mysql',
-    url: 'mysql://agrocontroldba:AdminAgrontrol7699*@localhost:3306/agrocontrol',
+    url: 'mysql://agrocontroldba:AdminAgrocontrol7699*@localhost:3306/agrocontrol',
     timezone: '+0',
   }
 };
