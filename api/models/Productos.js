4d64bfe3a42a6d994e21064a72a51a574812d872
Pablo Uribe <sistemaspablo21@gmail.com>
2024-05-23 01:09:09 -0500

    listar y filtrar productos

---
+++ api/models/Productos.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+/**
+ * Productos.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+    datastore: 'agrocontroldb',
+  
+    attributes: {
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+   
+      nombre: { type: 'string', required: true },
+  
+      valorestimado: { type: 'number', required: true },
+
+      tipo: { type: 'string', required: true },
+  
+      descripcion: { type: 'string', required: true },
+  
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  
+      user_id: { model: 'User' },
+    },
+  
+    customToJSON: function () {
+      //this.fecha = this.fecha.toISOString().split('T')[0];
+  
+      return this;
+    },
+  };
+  
